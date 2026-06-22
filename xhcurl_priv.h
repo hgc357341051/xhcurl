804e774610085ba1e1ca42db889c92f46471cacf
小胡 <357341051@qq.com>
2026-06-22 09:21:47 +0800

    RUST

--- xhcurl_priv.h
+++ xhcurl_priv.h
@@ -18,6 +18,10 @@
 #include "zend_exceptions.h"
 /* 引入 PHP SAPI 头文件（sapi_module 全局变量） */
 #include "SAPI.h"
+/* php_raw_url_encode 函数声明在 ext/standard/php_url.h 中
+ * macOS clang 将隐式函数声明视为错误（-Wimplicit-function-declaration），
+ * 必须显式包含该头文件 */
+#include "ext/standard/php_url.h"
 
 /* +----------------------------------------------------------------------+
  * | 平台兼容性宏定义                                                      |
